784f4ab5e8280ddbad335a8c3830f98450bc2f8e
timothy <r10922188@csie.ntu.edu.tw>
2025-06-18 11:32:35 +0800

    chore: CORS for debug

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/timothychen1999/vrcontrol-server/routes"
+	"github.com/timothychen1999/vrcontrol-server/utilities"
 )
 
 func main() {
@@ -23,6 +24,8 @@ func createRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+	router.Use(utilities.CORSall)
+
 	ws := router.Group("/ws")
 	routes.SetClientWsRoutes(ws)
 	simple := router.Group("/simple")
